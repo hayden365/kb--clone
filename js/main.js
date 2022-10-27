c09544d3db09bcbe012b1918d610cc45c4bdcca8
Hayden365 <ysmh100@gmail.com>
2022-10-27 19:17:14 +0900

    feat: mid_slide swiper 짤림 해결... spacebetween, padding, margin조정.

--- js/main.js
+++ js/main.js
@@ -12,16 +12,35 @@ function uiIntroSetting() {
   });
 }
 
-new Swiper('.mid_slide', {
+const midSwiper = new Swiper('.mid_slide', {
   slidesPerView: 3,
-  // autoplay: {
-  //   delay: 3000,
-  // },
-  spaceBetween: 20,
+  autoplay: {
+    delay: 5000,
+  },
+  spaceBetween: 28,
   centeredSlides: true,
   loop: true,
   navigation: {
     nextEl: '.next',
     prevEl: '.prev',
   },
+});
+
+const swiperPlay = document.querySelector('.play');
+const swiperPause = document.querySelector('.pause');
+swiperPlay.addEventListener('click', () => {
+  swiperPause.classList.add('__off');
+  swiperPlay.classList.remove('__off');
+  midSwiper.autoplay.start();
+});
+swiperPause.addEventListener('click', () => {
+  swiperPause.classList.remove('__off');
+  swiperPlay.classList.add('__off');
+  midSwiper.autoplay.stop();
+});
+
+const ui_toggle = document.querySelector('.ui-toggle');
+const ui_toggle_on = document.querySelector('ui-toggle.on');
+ui_toggle.addEventListener('click', () => {
+  ui_toggle.classList.remove('on');
 });
