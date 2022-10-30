2e2c1159675983fe64a71fead58fefc7860f9ee8
Hayden365 <ysmh100@gmail.com>
2022-10-30 19:05:10 +0900

    tag name수정

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
-function uiIntroSetting() {
-  // 상단 슬라이더
-  $('.wrap-visual').moveContents({
-    effect: 'show',
-    autoStop: true,
-    eventEl: '.area-control > ul > li > a',
-    conEl: '.area-con > ul > li',
-    changeTimer: 5000,
-    changeCallBack: function (a) {
-      $('.wrap-visual').attr('class', 'wrap-visual active' + (a * 1 + 1));
-    },
-  });
-}
+// function uiIntroSetting() {
+//   // 상단 슬라이더
+//   $('.wrap-visual').moveContents({
+//     effect: 'show',
+//     autoStop: true,
+//     eventEl: '.area-control > ul > li > a',
+//     conEl: '.area-con > ul > li',
+//     changeTimer: 5000,
+//     changeCallBack: function (a) {
+//       $('.wrap-visual').attr('class', 'wrap-visual active' + (a * 1 + 1));
+//     },
+//   });
+// }
 
 const midSwiper = new Swiper('.mid_slide', {
   slidesPerView: 3,
